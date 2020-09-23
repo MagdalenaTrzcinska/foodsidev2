2a47e2b8bb352e3953e51a257b176d6efb9f2649
Madga Trzcinska <magdatrzcinska23@gmail.com>
2020-09-23 20:35:33 +0200

    initial commit

--- main.js
+++ main.js
@@ -1,41 +1,42 @@
-setInterval(pojawianie, 3000);
-function pojawianie() {
-    const text=document.getElementsByClassName('testimonial__opinion1')[0];
-    const text2=document.getElementsByClassName('testimonial__opinion2')[0];
+setInterval(changingOpinions, 3000);
 
-    if(text.style.opacity==='1') {
+function changingOpinions() {
+    const text = document.getElementsByClassName('testimonial__opinion1')[0];
+    const text2 = document.getElementsByClassName('testimonial__opinion2')[0];
+
+    if (text.style.opacity === '1') {
         text.style.opacity = '0';
-        text2.style.opacity='1';
-    }
-    else {
+        text2.style.opacity = '1';
+    } else {
         text.style.opacity = '1';
         text2.style.opacity = '0';
     }
 }
+
 function onOpenMobileNav() {
     const container = document.querySelector('.forMobile');
 
-    if(container.style.display === 'block'){
-        container.style.display='none';
-    }
-    else{
-        container.style.display='block';
+    if (container.style.display === 'block') {
+        container.style.display = 'none';
+    } else {
+        container.style.display = 'block';
     }
 }
 
-setInterval(zmiana, 4000);
-function zmiana() {
+setInterval(changeOfPhotos, 4000);
 
-    var photo=document.getElementsByClassName('photomain')[0];
+function changeOfPhotos() {
 
-    if(photo.classList.contains('photo1')){
-        photo.style.backgroundImage= 'url(img/hero_1.jpg)';
+    var photo = document.getElementsByClassName('photomain')[0];
+
+    if (photo.classList.contains('photo1')) {
+        photo.style.backgroundImage = 'url(img/hero_1.jpg)';
         photo.classList.remove('photo1');
-    } else{
-        photo.style.backgroundImage= 'url(img/hero_2.jpg)';
+    } else {
+        photo.style.backgroundImage = 'url(img/hero_2.jpg)';
         photo.classList.add('photo1');
     }
-    document.getElementsByClassName('photomain')[0].style.backgroundImage=photo;
+    document.getElementsByClassName('photomain')[0].style.backgroundImage = photo;
 }
 
 
